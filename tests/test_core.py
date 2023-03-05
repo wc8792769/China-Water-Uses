2936b8c443b4bc4c9dbd3ffa1cd251baca410559
SongshGeo <songshgeo@gmail.com>
2023-03-05 21:03:12 +0800

    feat(ChineseWater): :sparkles: select data by scopes

---
+++ tests/test_core.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python 3.11.0
+# -*-coding:utf-8 -*-
+# @Author  : Shuang (Twist) Song
+# @Contact   : SongshGeo@gmail.com
+# GitHub   : https://github.com/SongshGeo
+# Website: https://cv.songshgeo.com/
+
+from src.core import ChineseWater
+
+
+def test_change_scopes():
+    """测试更新范围数据关心的范围"""
+    cw = ChineseWater()
+    assert cw.data.shape == (16709, 63)
+    cw.update_scope("sectors", include=["IRR", "IND"], exclude=["IND"])
+    assert cw.measurements == {"Magnitude", "WU", "WUI"}
+    assert cw.sectors == {"IRR"}
+    # 更新时间
+    cw.update_scope("Year", list(range(1966, 1978)))
+    assert cw.data.shape == (4092, 18)
+    cw.update_scope("Province_n", ["Anhui", "Chongqing"])
+    assert cw.data.shape == (204, 18)
